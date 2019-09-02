bc0ac030ac3da9f9494a6d95e20eafeb62fa25b2
Coda <a.ardroudi@gmail.com>
2019-09-02 17:42:31 +0430

    Initial commit

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+
+    private void Awake()
+    {
+        Transform point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right;
+
+        point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right * 2f;
+
+
+    }
+}
